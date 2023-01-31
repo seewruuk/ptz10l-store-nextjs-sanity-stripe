c38a22ccbf8d57d811d56794a2bdffb4b6d6cc6b
seewruuk <sewruczekk@icloud.com>
2023-01-31 13:13:32 +0100

    aezakmi

--- pages/przeczytajcie.js
+++ pages/przeczytajcie.js
@@ -19,7 +19,18 @@ const Czerwona = () => {
     }, [])
 
     return (
-        <></>
+        <div
+            style={{
+                position: "absolute",
+                top: "0",
+                left: "0",
+                right: "0",
+                bottom: "0",
+                height: "100%",
+                width: "100%",
+                background: "white"
+            }}
+        ></div>
     )
 }
 
